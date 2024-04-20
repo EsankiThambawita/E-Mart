2248b36c2d26b9ca41bb0020beb62e97f2c0bab7
sandali45 <139307298+sandali45@users.noreply.github.com>
2024-04-20 19:56:04 +0530

    Update UserDao.java
    Updated UserDao.java class in model package,

--- E-Mart/src/java/Model/userDao.java
+++ E-Mart/src/java/Model/userDao.java
@@ -8,9 +8,11 @@ package Model;
  *
  * @author DELL
  */
+import Model.SignUpUser;
 import java.sql.Connection; //it is sql not jms
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -37,5 +39,37 @@ public class userDao {
         }
     
     }
+
+    public SignUpUser getUserByEmail(String email) {
+      String driver = "com.mysql.cj.jdbc.Driver";
+    String url = "jdbc:mysql://localhost:3306/userdetails";
+    String sql = "SELECT * FROM usersignup WHERE email = ?";
+    SignUpUser user = null;
     
+    try {
+        Class.forName(driver);
+        Connection connection = DriverManager.getConnection(url, "root", "");
+        
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, email);
+            
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    String username = resultSet.getString("username");
+                    String password = resultSet.getString("password");
+                    String contactNumber = resultSet.getString("contact_number");
+                    
+                    // Create a new User object with fetched data
+                    user = new SignUpUser(username, email, password, contactNumber);
+                }
+            }
+        }
+    } catch (ClassNotFoundException | SQLException ex) {
+        Logger.getLogger(userDao.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
+    return user;
+    }
+
+   
 }
