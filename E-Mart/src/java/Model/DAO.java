6cfaba3b8a773fbacff3fc9369d5d32d2e4557e3
Rovin Fernando <135942955+TPR-Fernando@users.noreply.github.com>
2024-04-23 22:13:02 +0530

    Connected the shoppingCart to the database and it functions

--- E-Mart/src/java/Model/DAO.java
+++ E-Mart/src/java/Model/DAO.java
@@ -4,6 +4,7 @@
  */
 package Model;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +13,14 @@ import java.util.List;
  * @author robin
  */
 public class DAO {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/emart";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 
     public static List<Smartphone> getAllSmartphones() {
         return generateMockDataSmartphone();
@@ -37,39 +46,94 @@ public class DAO {
         return generateMockDataMonitor();
         //TODO: add real sql code
     }
-    
-    public static List<ShoppingCartObj> currentCartSnap = new ArrayList<>();
 
+    //public static List<ShoppingCartObj> currentCartSnap = new ArrayList<>();
     public static List<ShoppingCartObj> getAllCartItems() {
+        List<ShoppingCartObj> currentCartSnap = new ArrayList<>();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establishing connection to the database
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            // Creating SQL statement
+            statement = connection.createStatement();
+
+            // SQL query to fetch shopping cart items
+            String query = "SELECT * FROM ShoppingCart";
+
+            // Executing the query
+            resultSet = statement.executeQuery(query);
+
+            // Iterating over the result set and adding items to the shopping cart list
+            while (resultSet.next()) {
+                String productName = resultSet.getString("productName");
+                int productPrice = resultSet.getInt("productPrice");
+                int quantity = resultSet.getInt("quantity");
+                String description = resultSet.getString("description");
+                ProductCategory category = StringToProductCategoryConverter.convert(resultSet.getString("category"));
+                String iconPath = resultSet.getString("iconPath");
+                int recordId = resultSet.getInt("recordId");
+                String email = resultSet.getString("email");
+
+                // Creating ShoppingCartItem object and adding it to the list
+                ShoppingCartObj cartItem = new ShoppingCartObj(productName, productPrice, quantity, description,
+                        category, iconPath, recordId, email);
+                currentCartSnap.add(cartItem);
+            }
+        } catch (Exception e) {
+            System.err.println("Error while retrieving shopping cart items: " + e.getMessage());
+        } finally {
+            // Closing the connection, statement, and result set
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println("Error while retrieving shopping cart items: " + e.getMessage());
+            }
+        }
+
         return currentCartSnap;
-        //TODO: add real sql code
-    }
-    
-    public static void fillMockCart(){
-        currentCartSnap = generateMockShoppingCartObjs();
     }
 
+//    public static void fillMockCart() {
+//        currentCartSnap = generateMockShoppingCartObjs();
+//    }
     public static void removeAllCartItems() {
-        currentCartSnap.clear();
+        try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement("DELETE FROM shoppingcart")) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error while retrieving shopping cart items: " + e.getMessage());
+        }
     }
-    
-    public static List<ShoppingCartObj> getOrderConfirmationDetails(){
+
+    public static List<ShoppingCartObj> getOrderConfirmationDetails() {
         return generateMockShoppingCartObjs();
     }
-    
-    public static List<NewestProductObj> getNewArrivals(){
+
+    public static List<NewestProductObj> getNewArrivals() {
         return generateMockNewestItems();
-    } 
+    }
 
     //Mock Data
     private static List<Smartphone> generateMockDataSmartphone() {
         List<Smartphone> products = new ArrayList<>();
 
         // Mock data for smartphones
-        products.add(new Smartphone(1, "Smartphone A", 799.99f, 100,
+        products.add(new Smartphone(1, "Smartphone A", 799, 100,
                 new ArrayList<>(), "Brand X", "Model 1", "High-end smartphone with great features", "Smartphone",
                 "512GB", 6.5, "Black"));
-        products.add(new Smartphone(2, "Smartphone B", 699.99f, 150,
+        products.add(new Smartphone(2, "Smartphone B", 699, 150,
                 new ArrayList<>(), "Brand Y", "Model 2", "Affordable smartphone with good performance", "Smartphone",
                 "256GB", 6.0, "Silver"));
 
@@ -80,10 +144,10 @@ public class DAO {
         List<Laptop> products = new ArrayList<>();
 
         // Mock data for laptops
-        products.add(new Laptop(3, "Laptop A", 1299.99f, 50,
+        products.add(new Laptop(3, "Laptop A", 1299, 50,
                 new ArrayList<>(), "Brand Z", "Model 3", "Powerful laptop for professional use", "Laptop",
                 "1TB SSD", "Intel i7", "16GB"));
-        products.add(new Laptop(4, "Laptop B", 999.99f, 80,
+        products.add(new Laptop(4, "Laptop B", 999, 80,
                 new ArrayList<>(), "Brand W", "Model 4", "Mid-range laptop suitable for everyday tasks", "Laptop",
                 "512GB SSD", "Intel i5", "8GB"));
         return products;
@@ -92,10 +156,10 @@ public class DAO {
     private static List<Camera> generateMockDataCamera() {
         List<Camera> products = new ArrayList<>();
         // Mock data for cameras
-        products.add(new Camera(5, "Camera A", 599.99f, 30,
+        products.add(new Camera(5, "Camera A", 599, 30,
                 new ArrayList<>(), "Brand V", "Model 5", "Compact camera for photography enthusiasts", "Camera",
                 "Compact"));
-        products.add(new Camera(5, "Camera B", 399.99f, 20,
+        products.add(new Camera(5, "Camera B", 399, 20,
                 new ArrayList<>(), "Brand X", "Model 6", "Compact camera for photography enthusiasts", "Camera",
                 "Compact"));
         return products;
@@ -104,10 +168,10 @@ public class DAO {
     private static List<Smartwatch> generateMockDataSmartwatches() {
         List<Smartwatch> products = new ArrayList<>();
         // Mock data for smartwatches
-        products.add(new Smartwatch(6, "Smartwatch A", 299.99f, 80,
+        products.add(new Smartwatch(6, "Smartwatch A", 299, 80,
                 new ArrayList<>(), "Brand U", "Model 6", "Feature-rich smartwatch for fitness tracking", "Smartwatch",
                 "1.3 inches", "Red"));
-        products.add(new Smartwatch(7, "Smartwatch B", 199.99f, 120,
+        products.add(new Smartwatch(7, "Smartwatch B", 199, 120,
                 new ArrayList<>(), "Brand T", "Model 7", "Affordable smartwatch with basic features", "Smartwatch",
                 "1.1 inches", "Blue"));
         return products;
@@ -116,10 +180,10 @@ public class DAO {
     private static List<Monitor> generateMockDataMonitor() {
         List<Monitor> products = new ArrayList<>();
         // Mock data for monitors
-        products.add(new Monitor(8, "Monitor A", 399.99f, 40,
+        products.add(new Monitor(8, "Monitor A", 399, 40,
                 new ArrayList<>(), "Brand S", "Model 8", "High-quality monitor for gaming and multimedia", "Monitor",
                 144, "27 inches", "2560x1440"));
-        products.add(new Monitor(9, "Monitor B", 249.99f, 60,
+        products.add(new Monitor(9, "Monitor B", 249, 60,
                 new ArrayList<>(), "Brand R", "Model 9", "Budget-friendly monitor for everyday use", "Monitor",
                 60, "24 inches", "1920x1080"));
         return products;
@@ -128,23 +192,23 @@ public class DAO {
     private static List<ShoppingCartObj> generateMockShoppingCartObjs() {
         List<ShoppingCartObj> cartProducts = new ArrayList<>();
 
-        cartProducts.add(new ShoppingCartObj("Monitor A", 399.99, 1,
-                "34 inches", ProductCategory.Monitor, "Images/Home/monitor.png"));
-        cartProducts.add(new ShoppingCartObj("Smartphone X", 899.99, 2,
-                "256GB", ProductCategory.Smartphone, "Images/Home/phone.png"));
-        cartProducts.add(new ShoppingCartObj("Laptop Y", 1299.99, 1,
-                "512GB", ProductCategory.Laptop, "Images/Home/laptop.png"));
+        cartProducts.add(new ShoppingCartObj("Monitor A", 399, 1,
+                "34 inches", ProductCategory.Monitor, "Images/Home/monitor.png", 1, "boy@gmail.com"));
+        cartProducts.add(new ShoppingCartObj("Smartphone X", 899, 2,
+                "256GB", ProductCategory.Smartphone, "Images/Home/phone.png", 2, "girl@gmail.com"));
+        cartProducts.add(new ShoppingCartObj("Laptop Y", 1299, 1,
+                "512GB", ProductCategory.Laptop, "Images/Home/laptop.png", 3, "bi@gmail.com"));
         return cartProducts;
     }
-    
-    private static List<NewestProductObj> generateMockNewestItems(){
+
+    private static List<NewestProductObj> generateMockNewestItems() {
         List<NewestProductObj> newestItems = new ArrayList<>();
-        
+
         newestItems.add(new NewestProductObj(5, "Laptop Beta", 150000, "Images/ProductCategory&Details/Laptops/AcerChromebookSpin.png"));
-        newestItems.add(new NewestProductObj(1, "Monitor A", 399.99, "Images/ProductCategory&Details/Monitors/AsusProArtDisplayPA278CV.png"));
-        newestItems.add(new NewestProductObj(2, "Smartphone X", 899.99, "Images/ProductCategory&Details/Phones/i14p.png"));
-        newestItems.add(new NewestProductObj(3, "Laptop Y", 1299.99, "Images/ProductCategory&Details/Laptops/HPSpectre.png"));
-        newestItems.add(new NewestProductObj(4, "Laptop Z", 1099.99, "Images/ProductCategory&Details/Laptops/AcerChromebookSpin.png")); 
+        newestItems.add(new NewestProductObj(1, "Monitor A", 250000, "Images/ProductCategory&Details/Monitors/AsusProArtDisplayPA278CV.png"));
+        newestItems.add(new NewestProductObj(2, "Smartphone X", 100000, "Images/ProductCategory&Details/Phones/i14p.png"));
+        newestItems.add(new NewestProductObj(3, "Laptop Y", 250000, "Images/ProductCategory&Details/Laptops/HPSpectre.png"));
+        newestItems.add(new NewestProductObj(4, "Laptop Z", 350000, "Images/ProductCategory&Details/Laptops/AcerChromebookSpin.png"));
 
         return newestItems;
     }
