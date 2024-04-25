f8725216a2a2a84a87abdb59a1486b64dfcc9d30
Rovin Fernando <135942955+TPR-Fernando@users.noreply.github.com>
2024-04-25 20:40:29 +0530

    Fixed DAO and changes on ongoing database changes on index.jsp

--- E-Mart/src/java/Model/DAO.java
+++ E-Mart/src/java/Model/DAO.java
@@ -7,17 +7,6 @@ package Model;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-<<<<<<< Updated upstream
-import java.util.logging.Level;
-import java.util.logging.Logger;
-=======
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-
->>>>>>> Stashed changes
 
 /**
  *
@@ -96,31 +85,6 @@ public class DAO {
         return products;
     }
 
-<<<<<<< Updated upstream
-    public static List<Laptop> getAllLaptops() {
-        return generateMockDataLaptop();
-        //TODO: add real sql code
-    }
-
-    public static List<Camera> getAllCamera() {
-        return generateMockDataCamera();
-        //TODO: add real sql code
-    }
-
-    public static List<Smartwatch> getAllSmartwatches() {
-        return generateMockDataSmartwatches();
-        //TODO: add real sql code
-    }
-
-    public static List<Monitor> getAllMonitor() {
-        return generateMockDataMonitor();
-        //TODO: add real sql code
-    }
-<<<<<<< Updated upstream
-=======
-    
-    public static List<ShoppingCartObj> currentCartSnap = new ArrayList<>();
-=======
     //TODO
 //    public static List<Laptop> getAllLaptops() {
 //        return generateMockDataLaptop();
@@ -141,10 +105,7 @@ public class DAO {
 //        return generateMockDataMonitor();
 //        //TODO: add real sql code
 //    }
->>>>>>> Stashed changes
->>>>>>> Stashed changes
-
-    //public static List<ShoppingCartObj> currentCartSnap = new ArrayList<>();
+    
     public static List<ShoppingCartObj> getAllCartItems() {
         List<ShoppingCartObj> currentCartSnap = new ArrayList<>();
         Connection connection = null;
@@ -203,9 +164,7 @@ public class DAO {
         return currentCartSnap;
     }
 
-//    public static void fillMockCart() {
-//        currentCartSnap = generateMockShoppingCartObjs();
-//    }
+
     public static void removeAllCartItems() {
         try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement("DELETE FROM shoppingcart")) {
             statement.executeUpdate();
@@ -219,136 +178,58 @@ public class DAO {
     }
 
     public static List<NewestProductObj> getNewArrivals() {
-        return generateMockNewestItems();
+        List<NewestProductObj> newitems = new ArrayList<>();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establishing connection to the database
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            // Creating SQL statement
+            statement = connection.createStatement();
+
+            // SQL query to fetch shopping cart items
+            String query = "SELECT * FROM smartphone";
+
+            // Executing the query
+            resultSet = statement.executeQuery(query);
+
+            // Iterating over the result set and adding items to the shopping cart list
+            while (resultSet.next()) {
+                int productId = resultSet.getInt("productId");
+                String productName = resultSet.getString("productName");
+                int productPrice = resultSet.getInt("productPrice");
+                String iconPath = resultSet.getString("iconPath");
+
+                // Creating ShoppingCartItem object and adding it to the list
+                NewestProductObj items = new NewestProductObj(productId, productName, productPrice, iconPath);
+                newitems.add(items);
+            }
+        } catch (Exception e) {
+            System.err.println("Error while retrieving shopping cart items: " + e.getMessage());
+        } finally {
+            // Closing the connection, statement, and result set
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println("Error while retrieving shopping cart items: " + e.getMessage());
+            }
+        }
+        return newitems;
     }
 
-    //Mock Data
-    private static List<Smartphone> generateMockDataSmartphone() {
-        List<Smartphone> products = new ArrayList<>();
-
-        // Mock data for smartphones
-<<<<<<< Updated upstream
-        products.add(new Smartphone(1, "Smartphone A", 799, 100,
-=======
-<<<<<<< Updated upstream
-        products.add(new Smartphone(1, "Smartphone A", 799.99f, 100,
->>>>>>> Stashed changes
-                new ArrayList<>(), "Brand X", "Model 1", "High-end smartphone with great features", "Smartphone",
-                "512GB", 6.5, "Black"));
-        products.add(new Smartphone(2, "Smartphone B", 699, 150,
-                new ArrayList<>(), "Brand Y", "Model 2", "Affordable smartphone with good performance", "Smartphone",
-=======
-        products.add(new Smartphone(1, "Smartphone A", 799, 100,
-                "Images/Home/monitor.png", "Brand X", "Model 1", "High-end smartphone with great features", "Smartphone",
-                "512GB", 6.5, "Black"));
-        products.add(new Smartphone(2, "Smartphone B", 699, 150,
-                "Images/Home/monitor.png", "Brand Y", "Model 2", "Affordable smartphone with good performance", "Smartphone",
->>>>>>> Stashed changes
-                "256GB", 6.0, "Silver"));
-
-        return products;
-    }
-
-<<<<<<< Updated upstream
-    private static List<Laptop> generateMockDataLaptop() {
-        List<Laptop> products = new ArrayList<>();
-
-        // Mock data for laptops
-        products.add(new Laptop(3, "Laptop A", 1299, 50,
-                new ArrayList<>(), "Brand Z", "Model 3", "Powerful laptop for professional use", "Laptop",
-                "1TB SSD", "Intel i7", "16GB"));
-        products.add(new Laptop(4, "Laptop B", 999, 80,
-                new ArrayList<>(), "Brand W", "Model 4", "Mid-range laptop suitable for everyday tasks", "Laptop",
-                "512GB SSD", "Intel i5", "8GB"));
-        return products;
-    }
-
-    private static List<Camera> generateMockDataCamera() {
-        List<Camera> products = new ArrayList<>();
-        // Mock data for cameras
-        products.add(new Camera(5, "Camera A", 599, 30,
-                new ArrayList<>(), "Brand V", "Model 5", "Compact camera for photography enthusiasts", "Camera",
-                "Compact"));
-        products.add(new Camera(5, "Camera B", 399, 20,
-                new ArrayList<>(), "Brand X", "Model 6", "Compact camera for photography enthusiasts", "Camera",
-                "Compact"));
-        return products;
-    }
-
-    private static List<Smartwatch> generateMockDataSmartwatches() {
-        List<Smartwatch> products = new ArrayList<>();
-        // Mock data for smartwatches
-        products.add(new Smartwatch(6, "Smartwatch A", 299, 80,
-                new ArrayList<>(), "Brand U", "Model 6", "Feature-rich smartwatch for fitness tracking", "Smartwatch",
-                "1.3 inches", "Red"));
-        products.add(new Smartwatch(7, "Smartwatch B", 199, 120,
-                new ArrayList<>(), "Brand T", "Model 7", "Affordable smartwatch with basic features", "Smartwatch",
-                "1.1 inches", "Blue"));
-        return products;
-    }
-
-    private static List<Monitor> generateMockDataMonitor() {
-        List<Monitor> products = new ArrayList<>();
-        // Mock data for monitors
-        products.add(new Monitor(8, "Monitor A", 399, 40,
-                new ArrayList<>(), "Brand S", "Model 8", "High-quality monitor for gaming and multimedia", "Monitor",
-                144, "27 inches", "2560x1440"));
-        products.add(new Monitor(9, "Monitor B", 249, 60,
-                new ArrayList<>(), "Brand R", "Model 9", "Budget-friendly monitor for everyday use", "Monitor",
-                60, "24 inches", "1920x1080"));
-        return products;
-    }
-=======
-//    private static List<Laptop> generateMockDataLaptop() {
-//        List<Laptop> products = new ArrayList<>();
-//
-//        // Mock data for laptops
-//        products.add(new Laptop(3, "Laptop A", 1299, 50,
-//                new ArrayList<>(), "Brand Z", "Model 3", "Powerful laptop for professional use", "Laptop",
-//                "1TB SSD", "Intel i7", "16GB"));
-//        products.add(new Laptop(4, "Laptop B", 999, 80,
-//                new ArrayList<>(), "Brand W", "Model 4", "Mid-range laptop suitable for everyday tasks", "Laptop",
-//                "512GB SSD", "Intel i5", "8GB"));
-//        return products;
-//    }
-//
-//    private static List<Camera> generateMockDataCamera() {
-//        List<Camera> products = new ArrayList<>();
-//        // Mock data for cameras
-//        products.add(new Camera(5, "Camera A", 599, 30,
-//                new ArrayList<>(), "Brand V", "Model 5", "Compact camera for photography enthusiasts", "Camera",
-//                "Compact"));
-//        products.add(new Camera(5, "Camera B", 399, 20,
-//                new ArrayList<>(), "Brand X", "Model 6", "Compact camera for photography enthusiasts", "Camera",
-//                "Compact"));
-//        return products;
-//    }
-//
-//    private static List<Smartwatch> generateMockDataSmartwatches() {
-//        List<Smartwatch> products = new ArrayList<>();
-//        // Mock data for smartwatches
-//        products.add(new Smartwatch(6, "Smartwatch A", 299, 80,
-//                new ArrayList<>(), "Brand U", "Model 6", "Feature-rich smartwatch for fitness tracking", "Smartwatch",
-//                "1.3 inches", "Red"));
-//        products.add(new Smartwatch(7, "Smartwatch B", 199, 120,
-//                new ArrayList<>(), "Brand T", "Model 7", "Affordable smartwatch with basic features", "Smartwatch",
-//                "1.1 inches", "Blue"));
-//        return products;
-//    }
-//
-//    private static List<Monitor> generateMockDataMonitor() {
-//        List<Monitor> products = new ArrayList<>();
-//        // Mock data for monitors
-//        products.add(new Monitor(8, "Monitor A", 399, 40,
-//                new ArrayList<>(), "Brand S", "Model 8", "High-quality monitor for gaming and multimedia", "Monitor",
-//                144, "27 inches", "2560x1440"));
-//        products.add(new Monitor(9, "Monitor B", 249, 60,
-//                new ArrayList<>(), "Brand R", "Model 9", "Budget-friendly monitor for everyday use", "Monitor",
-//                60, "24 inches", "1920x1080"));
-//        return products;
-//    }
->>>>>>> Stashed changes
-
+//MOCK DATA
     private static List<ShoppingCartObj> generateMockShoppingCartObjs() {
         List<ShoppingCartObj> cartProducts = new ArrayList<>();
 
@@ -360,6 +241,7 @@ public class DAO {
                 "512GB", ProductCategory.Laptop, "Images/Home/laptop.png", 3, "bi@gmail.com"));
         return cartProducts;
     }
+//
 
     private static List<NewestProductObj> generateMockNewestItems() {
         List<NewestProductObj> newestItems = new ArrayList<>();
@@ -373,41 +255,4 @@ public class DAO {
         return newestItems;
     }
 
-    
-    //Feedback
-    public static String getCustomerEmail(int userId) {
-        String email = null;
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-
-        try {
-            conn = DatabaseConnection.getConnection(); // Replace with your database connection method
-            String sql = "SELECT email FROM users WHERE user_id = ?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setInt(1, userId);
-            rs = stmt.executeQuery();
-            if (rs.next()) {
-                email = rs.getString("email");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            // Close resources
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (stmt != null) {
-                    stmt.close();
-                }
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return email;
-    }
 }
