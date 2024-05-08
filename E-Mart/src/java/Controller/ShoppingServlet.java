b51c4b7e218702b52f8b9b1f87ecc992eb25602c
Rovin Fernando <135942955+TPR-Fernando@users.noreply.github.com>
2024-05-09 00:39:29 +0530

    Fixed add to cart and buy now buttons
    To add filter by email feature

--- E-Mart/src/java/Controller/ShoppingServlet.java
+++ E-Mart/src/java/Controller/ShoppingServlet.java
@@ -1,4 +1,5 @@
 package Controller;
+
 import Model.DAO;
 import Model.Smartphone;
 import Model.Camera;
@@ -6,12 +7,12 @@ import Model.Laptop;
 import Model.Monitor;
 import Model.Smartwatch;
 
-
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.*;
 
 import java.sql.Connection;
@@ -23,95 +24,76 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.List;
 
-
-
 @WebServlet(name = "ShoppingServlet", value = "/ShoppingServlet")
 public class ShoppingServlet extends HttpServlet {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/emart";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String productID = request.getParameter("ID");
+        PrintWriter out = response.getWriter();
+        int productId = Integer.parseInt(request.getParameter("ID"));
         String action = request.getParameter("action");
         String category = request.getParameter("category");
-        
-        if(category.equals("smartphones")){
-            //Call the getAllSmartphones method from DAo class
-            List<Smartphone> smartphones = DAO.getAllSmartphones();
-            
-            //Iterate over the smartphones List and add each smartphone to the Shopping Cart
-            for (Smartphone smartphone : smartphones){
-                ShoppingServlet.addToShoppingCart(smartphone.getProductId(), smartphone.getQuantity(), smartphone.getPrice(), smartphone.getProductName(), smartphone.getCategory(), "example@email.com", smartphone.getProductDescription());
+        int quantity = Integer.parseInt(request.getParameter("quantity"));
+        int price = Integer.parseInt(request.getParameter("productPrice"));
+        String description = request.getParameter("description");
+        String iconPath = request.getParameter("iconPath");
+        String productName = request.getParameter("productName");
+
+        if (action.equals("buy")) {
+            addToCheckout(productId, quantity, price, productName, category, description, iconPath);
+            response.sendRedirect("Checkout.jsp");
+
+        } else if (action.equals("cart")) {
+            String email = (String) request.getSession().getAttribute("email");
+            if (email != null && !email.isEmpty()) {
+                // User is logged in, proceed to add item to cart
+                addToShoppingCart(productId, quantity, price, productName, category, description, iconPath, email);
+                response.sendRedirect("ShoppingCart.jsp");
+            } else {
+                // User is not logged in, redirect to login page or display a message
+                response.sendRedirect("SignIn.jsp");
             }
         }
-        else if (category.equals("laptop")){
-             // Call the getAllLaptops method from DAO class
-            List<Laptop> laptops = DAO.getAllLaptops();
-
-            // Iterate over the laptops list and add each laptop to the shopping cart
-            for (Laptop laptop : laptops) {
-                addToShoppingCart(laptop.getProductId(), laptop.getQuantity(), laptop.getPrice(), laptop.getProductName(), laptop.getCategory(), "example@email.com", laptop.getProductDescription());
-            }
-        }
-        else if (category.equals("camera")) {
-                // Call the getAllCameras method from DAO class
-                List<Camera> cameras = DAO.getAllCamera();
-
-                // Iterate over the cameras list and add each camera to the shopping cart
-                for (Camera camera : cameras) {
-                    addToShoppingCart(camera.getProductId(), camera.getQuantity(), camera.getPrice(), camera.getProductName(), camera.getCategory(), "example@email.com", camera.getProductDescription());
-                }
-        }
-        else if (category.equals("smartwatch")) {
-                // Call the getAllSmartwatches method from DAO class
-                List<Smartwatch> smartwatches = DAO.getAllSmartwatches();
-
-                // Iterate over the smartwatches list and add each smartwatch to the shopping cart
-                for (Smartwatch smartwatch : smartwatches) {
-                    addToShoppingCart(smartwatch.getProductId(), smartwatch.getQuantity(), smartwatch.getPrice(), smartwatch.getProductName(), smartwatch.getCategory(), "example@email.com", smartwatch.getProductDescription());
-                }
-        }
-        else if (category.equals("monitor")) {
-                // Call the getAllMonitors method from DAO class
-                List<Monitor> monitors = DAO.getAllMonitor();
-
-                // Iterate over the monitors list and add each monitor to the shopping cart
-                for (Monitor monitor : monitors) {
-                    addToShoppingCart(monitor.getProductId(), monitor.getQuantity(), monitor.getPrice(), monitor.getProductName(), monitor.getCategory(), "example@email.com", monitor.getProductDescription());
-                }
-            }
-        
-        
-        
-  
     }
-    
     //Define addToShoppingCartFunction
-    public static void addToShoppingCart(int productId, int quantity, double price, String productName, String category, String email, String description){
-        
+
+    public static void addToCheckout(int productId, int quantity, int productPrice, String productName, String category, String description, String iconPath) {
         Connection connection = null;
         PreparedStatement statement = null;
-        
-        try{
-            //Get connection
+
+        try {
+            // Get connection
             connection = getConnection();
-            
-            //Create SQL Query
-            String Query = "INSERT INTO shoppingcart (productId, quantity, productPrice, productName, category, email, description, iconPath) VALUES (?, ?, ?, ?, ?, ?, ?)";
-            
+
+            // Create SQL Query
+            String Query = "INSERT INTO shoppingcart (productId, quantity, productPrice, productName, category, description, iconPath) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
             statement = connection.prepareStatement(Query);
-            
-            //Set parameters
+
+            // Set parameters
             statement.setInt(1, productId);
             statement.setInt(2, quantity);
-            statement.setDouble(3, price);
+            statement.setInt(3, productPrice);
             statement.setString(4, productName);
             statement.setString(5, category);
-            statement.setString(6, email);
-            statement.setString(7, description);
-            
-        } catch (SQLException e){
+            statement.setString(6, description);
+            statement.setString(7, iconPath);
+
+            // Execute the query
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-                // Close the connection and statement
+            // Close the connection and statement
             try {
                 if (statement != null) {
                     statement.close();
@@ -123,24 +105,53 @@ public class ShoppingServlet extends HttpServlet {
                 e.printStackTrace(); // Handle or log the exception appropriately
             }
         }
-        
-        
     }
-    
-     private static final String URL = "jdbc:mysql://localhost:3306/emart";
-     private static final String USERNAME = "root";
-     private static final String PASSWORD = "";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    public static void addToShoppingCart(int productId, int quantity, int productPrice, String productName, String category, String description, String iconPath, String email) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            // Get connection
+            connection = getConnection();
+
+            // Create SQL Query
+            String Query = "INSERT INTO shoppingcart (productId, quantity, productPrice, productName, category, description, iconPath, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+            statement = connection.prepareStatement(Query);
+
+            // Set parameters
+            statement.setInt(1, productId);
+            statement.setInt(2, quantity);
+            statement.setInt(3, productPrice);
+            statement.setString(4, productName);
+            statement.setString(5, category);
+            statement.setString(6, description);
+            statement.setString(7, iconPath);
+            statement.setString(8, email);
+
+            // Execute the query
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the connection and statement
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace(); // Handle or log the exception appropriately
+            }
+        }
     }
-    
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
-
     }
 }
-
